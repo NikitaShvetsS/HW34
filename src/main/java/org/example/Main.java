e4f38379f92679f1905fbff95f7d060dfdf700d2
nikitasvec <cryohush@gmail.com>
2023-09-23 15:56:04 +0200

    hw

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,6 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 public class Main {
--- src/main/java/org/example/entities/Order.java
+++ src/main/java/org/example/entities/Order.java
@@ -1,6 +1,5 @@
 package org.example.entities;
 
-import org.example.entities.Product;
 
 import java.util.Date;
 import java.util.List;
